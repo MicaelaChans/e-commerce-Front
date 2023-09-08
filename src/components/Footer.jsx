5dc44695f83d64b0cbb0968c95b34355ee5ee52c
MachinYDraco <fede.mier.2000@gmail.com>
2023-09-08 17:21:23 -0300

    Home

--- src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-function Footer() {
-  return <h2>Aca va el footer</h2>;
-}
-
-export default Footer;
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
 function NotFound() {
-  return <h2>Error 404 - La página no existe</h2>;
+  return (
+    <div>
+      <h2>Error 404 - La página no existe</h2>
+      <h3>Do you want to go Home?</h3>
+      <Link to="/">
+        <i className="bi bi-house"></i>
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
