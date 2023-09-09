e5332e1b66b13c4573f385de4df66ee6d23a76b4
MachinYDraco <fede.mier.2000@gmail.com>
2023-09-09 20:50:48 -0300

    comit de fede

--- src/components/partials/NavBar.jsx
+++ src/components/partials/NavBar.jsx
@@ -6,32 +6,31 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">Drachen</Navbar.Brand>
+        <Navbar.Brand Link="/">Drachen</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <NavDropdown title="Our Products" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
+              <NavDropdown.Item Link="#action/3.1">
                 Wood Stoves
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item Link="#action/3.2">
                 Pellet Stoves
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
+              <NavDropdown.Item Link="#action/3.3">
                 Accessories
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item Link="#action/3.4">
                 All Products
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
         <Nav>
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/contactUs">Contacto</Nav.Link>
-          <Nav.Link href="/aboutUs">About Us</Nav.Link>
-
+          <Nav.Link Link="/login">Login</Nav.Link>
+          <Nav.Link Link="/contact">Contacto</Nav.Link>
+          <Nav.Link Link="/aboutUs">About Us</Nav.Link>
           <i className="bi bi-cart3 mt-2"></i>
         </Nav>
       </Container>
