e584b26239a0d72e0a150bf9f4e1b6f4713a4a19
MartinGestido <89423775+MartinGestido@users.noreply.github.com>
2023-09-11 14:33:06 -0300

    .

--- src/components/partials/Product.jsx
+++ src/components/partials/Product.jsx
@@ -6,27 +6,27 @@ import { useParams } from "react-router-dom";
 
 function Product() {
   const dispatch = useDispatch();
-  const product = useSelector((state) => state.product);
+  const product = useSelector((state) => state.products);
   const params = useParams();
+  
   useEffect(() => {
     const getOneProduct = async () => {
       const response = await axios({
         method: "GET",
-        url: "http://localhost:8000/products/64ff1f522c68a86939652721",
-      });
-      console.log("llegamos");
-      console.log(response.data);
+        url: `http://localhost:8000/products/${params.id}`,
+      });    
       dispatch(getProduct(response.data));
     };
     getOneProduct();
   }, []);
 
-  return (
+   console.log(product)
+   return  (
     <div>
-      <h1>lelgamos a product</h1>
+      <h1>{product.name}</h1>
       <h2>algo</h2>
     </div>
-  );
+  )
 }
 
 export default Product;
