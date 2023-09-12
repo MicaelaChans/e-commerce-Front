dd57ea7e04261ca2f66c6fd67b6b64d418953630
MartinGestido <89423775+MartinGestido@users.noreply.github.com>
2023-09-12 00:36:59 -0300

    .

--- src/components/partials/NavBar.jsx
+++ src/components/partials/NavBar.jsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 import { useState } from "react";
 import Cart from "../Cart";
@@ -10,26 +11,30 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">Drachen</Navbar.Brand>
+        <Navbar.Brand href="/"><img className="mb-3  image-navbar" src="./src/assets/drachen_logo_black.png" alt="drachen logo"/>  </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <NavDropdown title="Our Products" id="basic-nav-dropdown">
-              <NavDropdown.Item to="#action/3.1">Wood Stoves</NavDropdown.Item>
-              <NavDropdown.Item to="#action/3.2">
+            <NavDropdown className="mx-3" title="Our Products" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="#action/3.1">Wood Stoves</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#action/3.2">
                 Pellet Stoves
               </NavDropdown.Item>
-              <NavDropdown.Item to="#action/3.3">Accessories</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#action/3.3">Accessories</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item to="#action/3.4">All Products</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#action/3.4">All Products</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
         <Nav>
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/contact">Contacto</Nav.Link>
-          <Nav.Link href="/aboutUs">About Us</Nav.Link>
-          <Nav.Link href="/cart">
+        <NavDropdown title={<i className="bi bi-person-fill"></i>} id="basic-nav-dropdown">
+         <NavDropdown.Item as={Link} to={"/login"}>Login</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to={"/register"}>Sign up</NavDropdown.Item>
+          <NavDropdown.Item >Log out</NavDropdown.Item>
+        </NavDropdown>      
+          <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
+          <Nav.Link as={Link} to="/aboutUs">About Us</Nav.Link>
+          <Nav.Link as={Link} to="/cart">
             <i className="bi bi-cart3 mt-2"></i>
           </Nav.Link>
         </Nav>
