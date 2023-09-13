2bb5c16badea07f3eb4f6514ac4abe6ec0f7d27f
MartinGestido <89423775+MartinGestido@users.noreply.github.com>
2023-09-13 13:54:18 -0300

    .

--- src/components/partials/Hero.jsx
+++ src/components/partials/Hero.jsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <div className="hero-container">
       <div className="text-center">
-        <h2 className="hero-title App-header transition bignadpis">Drachen</h2>
+      { /* <h2 className="hero-title App-header transition bignadpis">Drachen</h2>*/}
       </div>
     </div>
   );
