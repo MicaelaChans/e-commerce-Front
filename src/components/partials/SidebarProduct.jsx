e7cb24ff810a73814cab73cf15b2a6cc733cce8e
facundogerman <f.g.centurion.p@gmail.com>
2023-09-13 12:43:31 -0300

    Product Page

---
+++ src/components/partials/SidebarProduct.jsx
@@ -0,0 +1,19 @@
+function SidebarProduct({ name, price, image }) {
+  return (
+    <div className="pt-5 p-4 border-start right-column-product">
+      <div className="details-right-column-product d-flex">
+        <div className="m-2">
+          <h5 className="text-uppercase">{name}</h5>
+          <p className="fw-semibold">US$ {price}</p>
+        </div>
+        <img
+          className="image-right-column-product ms-5"
+          src={image}
+          alt={name}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SidebarProduct;
