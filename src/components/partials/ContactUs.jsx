20e419bdde48037f0ad878e32eb61e884c44a4e5
Micaela Chans <micaelachansmoure@gmail.com>
2023-09-14 16:02:55 -0300

    .

--- src/components/partials/ContactUs.jsx
+++ src/components/partials/ContactUs.jsx
@@ -24,37 +24,25 @@ function ContactUs() {
           </div>
         </div>
       </div>
-      <div className="d-flex flex-column form ">
+      <div className="d-flex flex-column form w-100">
         <div className="row">
-          <div className="col-6">
+          <div className="col-lg-6 col-md-6 col-sm-12">
             <Form className="fs-3 ">
               <Form.Group className="mb-3" controlId="example">
                 <Form.Label>Name</Form.Label>
-                <Form.Control
-                  className=" border-0 formControl"
-                  type="email"
-                  placeholder="Mary"
-                />
+                <Form.Control className=" border-0 formControl" type="email" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="example">
                 <Form.Label>Email </Form.Label>
-                <Form.Control
-                  className=" border-0 formControl"
-                  type="email"
-                  placeholder="name@example.com"
-                />
+                <Form.Control className=" border-0 formControl" type="email" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="example">
                 <Form.Label>Phone</Form.Label>
-                <Form.Control
-                  className=" border-0 formControl"
-                  type="phone"
-                  placeholder="091 234 567"
-                />
+                <Form.Control className=" border-0 formControl" type="phone" />
               </Form.Group>
             </Form>
           </div>
-          <div className="col-6">
+          <div className="col-lg-6 col-md-6 col-sm-12">
             <Form className="fs-3 ">
               <Form.Group className="mb-3" controlId="example">
                 <Form.Label>Subject</Form.Label>
@@ -63,36 +51,35 @@ function ContactUs() {
                   type="subject"
                 />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="example">
+              <Form.Group className="mb-3 " controlId="example">
                 <Form.Label>Comment</Form.Label>
                 <Form.Control
                   className=" border-0 formControl"
                   as="textarea"
-                  placeholder="Leave us a comment..."
-                  rows={3}
+                  rows={5}
                 />
-                <div className="form-check fs-6">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckDefault"
-                  />
-                  <label
-                    className="form-check-label "
-                    htmlFor="flexCheckDefault"
-                  >
-                    I consent to the terms of the privacy politic
-                  </label>
-                </div>
+              </Form.Group>
+              <div className="form-check fs-6">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+                />
+                <label className="form-check-label " htmlFor="flexCheckDefault">
+                  I consent to the terms of the privacy politic
+                </label>
+              </div>
+              <Form.Group className="d-flex justify-content-end">
+                <button
+                  className="btn btn-outline-light shadow btnSend"
+                  type="button"
+                >
+                  Send
+                </button>
               </Form.Group>
             </Form>
           </div>
-          <Form.Group className="d-flex justify-content-end">
-            <button className="btn btn-dark shadow btnSend" type="button">
-              Send
-            </button>
-          </Form.Group>
         </div>
       </div>
       <Footer />
