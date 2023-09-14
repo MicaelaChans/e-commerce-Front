f19a1c716755b14894f06218eba15be6a905e894
facundogerman <f.g.centurion.p@gmail.com>
2023-09-14 16:06:46 -0300

    oneproduct

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -55,7 +55,7 @@ function ProductPage() {
                   <button
                     className="btn-product-page fw-semibold p-2"
                     onClick={() => handleAddItem(product)}
-                    style={{backgroundColor:"#f9b468"}}
+                    style={{ backgroundColor: "#f9b468" }}
                   >
                     Add to Cart
                   </button>
