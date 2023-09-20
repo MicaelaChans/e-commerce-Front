55bba2a5c94242dc89897dcaa11e3288f0281079
MachinYDraco <fede.mier.2000@gmail.com>
2023-09-20 14:53:18 -0300

    checkout

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -2,7 +2,7 @@ import "../styles/CheckOut.css";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-
+import { format } from "date-fns";
 function CheckOut() {
   const [orders, setOrders] = useState([]);
   const dispatch = useDispatch();
@@ -39,44 +39,103 @@ function CheckOut() {
       }
     }
   }
-
+  console.log(unpaidOrders);
   return (
     unpaidOrders && (
       <div className="checkout-container">
         <div className="container checkOut">
           <div className="row check-row">
-            <div className="col-8">
-              <ul>
+            <div className="col-12">
+              <div>
                 {unpaidOrders.map((order) => (
-                  <div className="order-item" key={order.id}>
-                    {order.products.map((product) => (
-                      <div className="product-item" key={product.id}>
-                        <img src={product.image} alt={product.name} />
-                        <div className="product-details">
-                          <h3>{product.name}</h3>
-                          <p>Price: ${product.price}</p>
-                          <p>Quantity: {product.length}</p>
+                  <div
+                    className="mt-3 border rounded-3 shadow p-3"
+                    key={order.id}
+                  >
+                    <div className="d-flex justify-content-between align-items-center ">
+                      <p className="mb-0">
+                        {format(new Date(order.createdAt), "MMMM dd yyyy")}
+                      </p>
+                      <p className="mb-0 order-reference">
+                        Order:{" "}
+                        <span style={{ fontWeight: "600" }}>{order.id}</span>{" "}
+                      </p>
+                    </div>
+                    <hr />
+                    <div className="order-item">
+                      {order.products.map((product) => (
+                        <div key={product.id}>
+                          <div className="product-item d-flex justify-content-around">
+                            <div>
+                              <img
+                                className="img-check-out"
+                                src={product.image}
+                                alt={product.name}
+                              />
+                            </div>
+                            <div className="product-details">
+                              <h3>{product.name}</h3>
+                            </div>
+
+                            <div>
+                              <h4>Unit price:</h4>
+                              <p>US${product.price}</p>
+                              <p>Quantity: 1</p>
+                            </div>
+                            <div>
+                              <h6>Total Price:</h6>
+                              <p>Q x P</p>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                    <div>
+                      <div className="payment-info d-flex justify-content-between">
+                        <div>
+                          <h4 className="mx-3">Payment & delivery Info:</h4>
+                        </div>
+                        <div>
+                          <h6>Total price:</h6>
+                        </div>
+                        <div>
+                          <p>Payment Method</p>
+                          <label style={{ display: "block" }}>
+                            <input
+                              type="radio"
+                              className="circular-radio"
+                              name="paymentMethod"
+                              value="creditCard"
+                            />{" "}
+                            Credit card
+                          </label>
+                          <label style={{ display: "block" }}>
+                            <input
+                              type="radio"
+                              className="circular-radio"
+                              name="paymentMethod"
+                              value="paypal"
+                            />
+                            Paypal
+                          </label>
+                        </div>
+                        <div>
+                          <h4>Total order price:</h4>
+                          <h6>US$ tanto</h6>
                         </div>
                       </div>
-                    ))}
-                    <button onClick={() => handlePay(order.id)}>
-                      Pay Order
-                    </button>
+
+                      <div className=" d-flex justify-content-end mt-2">
+                        <button
+                          className="btn btn-lg buy-button justify-content-end w-30"
+                          onClick={() => handlePay(order.id)}
+                        >
+                          Pay Order
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 ))}
-              </ul>
-            </div>
-            <div className="col-4">
-              <div className="payment-info">
-                <h2>Payment Info</h2>
-                <p>Payment Method</p>
-                <label>
-                  <input type="checkbox" className="circular-checkbox" /> Credit
-                  card
-                </label>
-                <label>
-                  <input type="checkbox" className="circular-checkbox" /> Paypal
-                </label>
               </div>
             </div>
           </div>
