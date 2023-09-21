a3f1cee87b09d3432407f0b53f301419b1d492e2
maurodenava01 <maurodenava01@gmail.com>
2023-09-21 17:05:39 -0300

    vista notFound arreglada

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 function NotFound() {
   return (
-    <div className="notFound">
+    <div className="notFound container">
       <h2>Error 404 - La página no existe</h2>
       <div className="d-flex align-items-center">
-        <Link to="/">
+        <Link to="/" className="text-decoration-none">
           <h3>
             Do you want to go Home? <i className="bi bi-house"></i>
           </h3>
