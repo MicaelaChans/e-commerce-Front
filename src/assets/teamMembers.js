c7e6a5bd72a623ef8fcfaa384e86af3f575ecf13
Micaela Chans <micaelachansmoure@gmail.com>
2023-09-27 00:32:34 -0300

    img

--- src/assets/teamMembers.js
+++ src/assets/teamMembers.js
@@ -1,7 +1,7 @@
 const teamMembers = [
   {
     key: 1,
-    profImg: "https://cdn-icons-png.flaticon.com/512/6596/6596121.png",
+    profImg: "./agustinMartinez.jpg",
     name: "Agustín Martínez",
     linkLi: "#",
     linkGi: "#",
@@ -22,21 +22,21 @@ const teamMembers = [
   },
   {
     key: 4,
-    profImg: "https://cdn-icons-png.flaticon.com/512/6596/6596121.png",
+    profImg: "./martinGestido.jpeg",
     name: "Martin Gestido",
     linkLi: "#",
     linkGi: "#",
   },
   {
     key: 5,
-    profImg: "https://cdn-icons-png.flaticon.com/512/6596/6596121.png",
+    profImg: "./mauroDeNava.png",
     name: "Mauro de Nava",
     linkLi: "https://www.linkedin.com/in/mauro-de-nava",
     linkGi: "https://github.com/maurodenava01",
   },
   {
     key: 6,
-    profImg: "https://cdn-icons-png.flaticon.com/512/6596/6596121.png",
+    profImg: "./micaelaChans.jpeg",
     name: "Micaela Chans",
     linkLi: "https://www.linkedin.com/in/micaela-chans/",
     linkGi: "https://github.com/MicaelaChans",
